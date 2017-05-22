421f5eb910adc46d43a1021cf3e4bf87eef33ebc
Sardo <sardo@pixelactionstudio.com>
2017-05-22 15:46:59 +0800

    Add backend code

--- js/widget.js
+++ js/widget.js
@@ -3,7 +3,14 @@
       window.hologram(document.getElementsByClassName('hologram-area')[0], {
           uploadUrl: '/hologram/hologram-server/',
           config: {
-              uploader: '/hologram/hologram-server/upload.php'
+              uploader: '/hologram/upload',
+              onComplete: function(result){
+                  try{
+                      
+                  }catch(ex){
+
+                  }
+              }
           },
       });
   });
