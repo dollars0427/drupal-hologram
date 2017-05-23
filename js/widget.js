c6d4b8cee8da8a73731b82f12d378e18328730e8
Sardo <sardo@pixelactionstudio.com>
2017-05-23 11:52:26 +0800

    Pass upload path to hologram by setting

--- js/widget.js
+++ js/widget.js
@@ -1,7 +1,7 @@
 (function ($) {
   $(window).load(function(){
       window.hologram(document.getElementsByClassName('hologram-area')[0], {
-          uploadUrl: '/sites/default/files/',
+          uploadUrl: Drupal.settings.Hologram.uploadUrl,
           onComplete: function(result){
               result = JSON.stringify(result);
               $('input[name="field_image[und][0][value][field]"').val(result);
