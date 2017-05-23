bfee81f0c93f662e736bb51e2489c74c8950cd46
Sardo <sardo@pixelactionstudio.com>
2017-05-23 10:50:33 +0800

    Widget.js will pass result to input field

--- js/widget.js
+++ js/widget.js
@@ -2,7 +2,8 @@
   $(window).load(function(){
       window.hologram(document.getElementsByClassName('hologram-area')[0], {
           onComplete: function(result){
-             //console.log(result);
+              result = JSON.stringify(result);
+              $('input[name="field_image[und][0][value][field]"').val(result);
          },
           config: {
               uploader: '/hologram/upload'
