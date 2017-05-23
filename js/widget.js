0db4b0354f2e3a3799fdab60e6edab508fd22eec
Sardo <sardo@pixelactionstudio.com>
2017-05-23 11:59:44 +0800

    Input field can sync with widget

--- js/widget.js
+++ js/widget.js
@@ -3,12 +3,20 @@
       window.hologram(document.getElementsByClassName('hologram-area')[0], {
           uploadUrl: Drupal.settings.Hologram.uploadUrl,
           onComplete: function(result){
-              result = JSON.stringify(result);
+              result = JSON.stringify(result['files']);
               $('input[name="field_image[und][0][value][field]"').val(result);
          },
           config: {
               uploader: '/hologram/upload'
           },
+      });
+
+      var store = window.hologram.store;
+
+      store.subscribe(function(){
+          var files = store.getState().files;
+          var val = JSON.stringify(files);
+          $('input[name="field_image[und][0][value][field]"').val(val);
       });
   });
 })(jQuery);
