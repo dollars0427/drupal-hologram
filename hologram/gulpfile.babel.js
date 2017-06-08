4003f7aa81e5abe33e6deedb79ec89d3dde0a400
Sardo <sardo@pixelactionstudio.com>
2017-06-08 14:56:18 +0800

    Add build css script

--- hologram/gulpfile.babel.js
+++ hologram/gulpfile.babel.js
@@ -2,14 +2,16 @@ import gulp from 'gulp';
 var gutil = require('gulp-util');
 var webpack = require('webpack');
 var webpackCfg = require('./webpack.config.js');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('default', ['build']);
 
 gulp.task('watch', function () {
-  gulp.watch('app/**/*.js', ['build']);
+  gulp.watch('app/**/*.js', ['build:js']);
+  gulp.watch('css/*.css', ['build:css']);
 });
 
-gulp.task('build', function () {
+gulp.task('build:js', function () {
     // run webpack
     webpack(webpackCfg, function(err, stats) {
         if(err) throw new gutil.PluginError('webpack', err);
@@ -20,3 +22,11 @@ gulp.task('build', function () {
     });
     console.log('build');
 });
+
+gulp.task('build:css', function(){
+    return gulp.src('./css/*.css')
+    .pipe(concatCss('Hologram.css'))
+    .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('build', ['build:css','build:js']);
