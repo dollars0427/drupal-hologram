f47d3b95f5829b3b0e701ab44f13614d1adff07a
Sardo Ip <sardo@pixelactionstudio.com>
2017-06-09 14:59:54 +0800

    Fix varriable name of json

--- js/widget.js
+++ js/widget.js
@@ -4,7 +4,6 @@
           uploadUrl: Drupal.settings.Hologram.uploadUrl,
           onComplete: function(result){
               var response = result['response'];
-              console.log(result['response'].text);
               var json = JSON.stringify(result['files']);
               $('input[name="field_image[und][0][value][image_json]"').val(json);
          },
@@ -17,7 +16,7 @@
 
       store.subscribe(function(){
           var files = store.getState().files;
-          var val = JSON.stringify(files);
+          var json = JSON.stringify(files);
           $('input[name="field_image[und][0][value][field][image_json]"').val(json);
       });
   });
