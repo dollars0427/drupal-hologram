2034f4b8610b4914a38660329392d8f5fc582289
Sardo Ip <sardo@pixelactionstudio.com>
2017-06-12 14:49:36 +0800

    Can display existed image of node by hologram widget

--- js/widget.js
+++ js/widget.js
@@ -1,23 +1,32 @@
 (function ($) {
   $(window).load(function(){
-      window.hologram(document.getElementsByClassName('hologram-area')[0], {
-          uploadUrl: Drupal.settings.Hologram.uploadUrl,
-          onComplete: function(result){
-              var response = result['response'];
-              var json = JSON.stringify(result['files']);
-              $('input[name="field_image[und][0][value][image_json]"').val(json);
-         },
-          config: {
-              uploader: '/hologram/upload'
-          },
-      });
+    window.hologram(document.getElementsByClassName('hologram-area')[0], {
+      uploadUrl: Drupal.settings.Hologram.uploadUrl,
+      onComplete: function(result){
+        var response = result['response'];
+        var json = JSON.stringify(result['files']);
+        $('input[name="field_image[und][0][value][image_json]"').val(json);
+      },
+      config: {
+        uploader: '/hologram/upload'
+      },
+    });
 
-      var store = window.hologram.store;
+    var store = window.hologram.store;
 
-      store.subscribe(function(){
-          var files = store.getState().files;
-          var json = JSON.stringify(files);
-          $('input[name="field_image[und][0][value][field][image_json]"').val(json);
-      });
+    //Push exist images to holgoram widget
+
+    try{
+      var files = JSON.parse($('input[name="field_image[und][0][value][image_json]').val());
+      store.dispatch(window.hologram.addFiles(files));
+    }catch(ex){
+      console.log(ex);
+    }
+
+    store.subscribe(function(){
+      var files = store.getState().files;
+      var json = JSON.stringify(files);
+      $('input[name="field_image[und][0][value][field][image_json]"').val(json);
+    });
   });
 })(jQuery);
