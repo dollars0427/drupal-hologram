aa2e9d2e9f8b0633c6ffc532d7fe9a1af8b7dc55
Sardo Ip <sardo@pixelactionstudio.com>
2017-06-12 16:33:18 +0800

    Improve json field beviover

--- js/widget.js
+++ js/widget.js
@@ -5,7 +5,7 @@
       onComplete: function(result){
         var response = result['response'];
         var json = JSON.stringify(result['files']);
-        $('input[name="field_image[und][0][value][image_json]"').val(json);
+        $('input[name="field_image[und][0][value][image_data]"').val(json);
       },
       config: {
         uploader: '/hologram/upload'
@@ -17,8 +17,11 @@
     //Push exist images to holgoram widget
 
     try{
-      var files = JSON.parse($('input[name="field_image[und][0][value][image_json]').val());
-      store.dispatch(window.hologram.addFiles(files));
+      var files = $('input[name="field_image[und][0][value][image_data]').val();
+      if(files){
+        files = JSON.parse(files);
+        store.dispatch(window.hologram.addFiles(files));
+      }
     }catch(ex){
       console.log(ex);
     }
@@ -26,7 +29,7 @@
     store.subscribe(function(){
       var files = store.getState().files;
       var json = JSON.stringify(files);
-      $('input[name="field_image[und][0][value][field][image_json]"').val(json);
+      $('input[name="field_image[und][0][value][field][image_data]"').val(json);
     });
   });
 })(jQuery);
