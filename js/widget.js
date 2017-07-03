e1465f5555de63dc68772949891a6d5b5d6f3331
Sardo <sardo@pixelactionstudio.com>
2017-07-03 16:02:50 +0800

    Store can pass file json to input field

--- js/widget.js
+++ js/widget.js
@@ -2,11 +2,11 @@
    $(window).load(function(){
       var fields = document.getElementsByClassName('field-widget-hologram-image');
       Array.prototype.forEach.call(fields, function(field){
+         var jsonField = $(field).find('input').get(0);
          var area = $(field).find('.hologram-area').get(0);
          var handle = window.hologram(area, {
             uploadUrl: Drupal.settings.Hologram.uploadUrl,
             onComplete: function(result){
-               var jsonField = $(field).find('input').get(0);
                var json = JSON.stringify(result);
                $(jsonField).val(json);
             },
@@ -16,6 +16,25 @@
          });
          handle.store
          handle.addFiles
+
+         //Push exist images to holgoram widget
+
+         try{
+            var files = $(jsonField).val();
+            if(files){
+               files = JSON.parse(files);
+               handle.store.dispatch(handle.addFiles(files));
+            }
+         }catch(ex){
+            console.log(ex);
+         }
+
+         handle.store.subscribe(function(){
+            var files = handle.store.getState().files;
+            var json = JSON.stringify(files);
+            $(jsonField).val(json);
+         });
+
       });
    });
 })(jQuery);
